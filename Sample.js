065d555de9e28f8e7313bd07fa21060c0331a099
crazykurt <kurtysulatj3@gmail.com>
2023-10-26 13:56:27 +0800

    first commit

---
+++ Sample.js
@@ -0,0 +1,12 @@
+{
+  "name": "John Doe",
+  "age": 30,
+  "city": "New York",
+  "email": "johndoe@example.com",
+  "hobbies": ["Reading", "Hiking", "Cooking"],
+  "address": {
+    "street": "123 Main St",
+    "zipcode": "10001",
+    "state": "NY"
+  }
+}
